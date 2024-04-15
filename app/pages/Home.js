18043637b7c97166daa2c8a2c851f82c395b4f52
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-04-15 20:49:59 +0700

    Kết nối firebase

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,9 +1,26 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
 import Header from "../components/HomeScreen/Header";
 import Slider from "../components/HomeScreen/Sliders";
+import { app } from "../../firebaseConfig";
+import React, { useEffect, useState } from "react";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
+  const db = getFirestore(app);
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const [userList, setUserList] = useState([]);
+  const getUsers = async () => {
+    setUserList([]);
+    const querySnapshot = await getDocs(collection(db, "User"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      setUserList((userList) => [...userList, doc.data()]);
+    });
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <Header />
