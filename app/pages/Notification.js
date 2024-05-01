89220e4fc0ed479bd172073cd0d54cf670f18a62
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-05-02 06:51:14 +0700

    Fix lỗi

--- app/pages/Notification.js
+++ app/pages/Notification.js
@@ -1,6 +1,8 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+// import { View, Text } from "react-native";
+// import React from "react";
+import React, { useCallback, useMemo, useRef } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 export default function Notification() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
