7bb78374c00fd70aec630a3f90c800a5546ac20f
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-05-08 23:24:45 +0700

    Fix lỗi

--- app/pages/JobDetail.js
+++ app/pages/JobDetail.js
@@ -341,7 +341,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     padding: 12,
-    backgroundColor: "#fffff",
+    backgroundColor: "white",
     justifyContent: "space-between",
     alignItems: "center",
     flexDirection: "row",
